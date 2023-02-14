ddc41161dc5d0941a3fb6a8b6ddd089c2f01c0ea
asifrazaansari <asifraza0231@gmail.com>
2023-02-14 21:26:20 +0530

    updated home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,30 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import axios from 'axios'
 import CardComp from '../../components/CardComp'
 
 const Home = ({ search }) => {
     const [jobs, setJobs] = useState([])
 
-    const getJobs = async () => {
-        const response = await axios.get("https://job-board.up.railway.app/jobs", {
-            params: {
-                title: search
-            }
-        })
-        setJobs(response.data.data)
-    }
+    const getJobs = useCallback(async () => {
+
+        try {
+            const response = await axios.get("https://job-board.up.railway.app/jobs", {
+                params: {
+                    title: search
+                }
+            })
+            setJobs(response.data.data)
+        } catch (error) {
+            //console.log(error)
+            setJobs([])
+        }
+
+    }, [search])
 
     useEffect(() => {
         getJobs()
-    }, [search])
+    }, [search, getJobs])
 
     return (
         <div className='container'>
             <h2>List of all Jobs</h2>
             {
-                jobs && jobs.map((job) => {
-                    return <CardComp key={job._id} link={`/jobs/${job._id}/apply`} text={"Apply"} job={job} />
-                })
+                jobs.length ? (
+                    jobs && jobs.map((job) => {
+                        return <CardComp key={job._id} link={`/jobs/${job._id}/apply`} text={"Apply"} job={job} />
+                    })
+                ) : (
+                    <p>No jobs found</p>
+                )
+                
             }
         </div>
     )
